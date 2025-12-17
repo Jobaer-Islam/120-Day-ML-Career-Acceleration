919cde58471ce4d9bd889085f06c05ecae6c866e
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-12-17 13:05:01 +0600

    Add Identity_Swap.py for swapping variables

---
+++ Week_01/Day - 01/Identity_Swap.py
@@ -0,0 +1,8 @@
+left_hand = "Coffee"
+right_hand = "Tea"
+
+# Swap them in one line!
+left_hand, right_hand = right_hand, left_hand
+
+print(f"Left Hand: {left_hand}")   # Tea
+print(f"Right Hand: {right_hand}") # Coffee
