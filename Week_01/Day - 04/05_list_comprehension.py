1d79d8c9e8eb94317cb342329c0b6ea03adfe9dd
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-12-18 23:02:39 +0600

    Added list comprehension example

---
+++ Week_01/Day - 04/05_list_comprehension.py
@@ -0,0 +1,9 @@
+"""
+Concept: List Comprehension
+Compact syntax for loops with conditions
+"""
+
+# Squares of even numbers from 1 to 10
+squares = [x**2 for x in range(1, 11) if x % 2 == 0]
+
+print("Squares of even numbers:", squares)
