d833104e3f7e6f6387f3d5fdd00668e770304e1d
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-12-18 22:59:19 +0600

    Add basic list operations example in Python

---
+++ Week_01/Day - 04/01_lists_basics.py
@@ -0,0 +1,10 @@
+"""
+Concept: List Basics
+Definition: A list is an ordered, mutable collection of values.
+"""
+
+numbers = [10, 20, 30, 40, 50]
+
+print("List:", numbers)
+print("First element:", numbers[0])
+print("Length of list:", len(numbers))
